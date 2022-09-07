6518d40262ecb50d6e688adc7ef25565a19a79a6
Yan <yangalperin@gmail.com>
2022-09-07 06:54:28 +0300

    task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Показать натуральные числа от N до 1, N задано
+int a = ReadInt("Enter first number: ");
+int n = ReadInt("Enter second number: ");
+string NumbersRec(int a, int n)
+{
+ if (a <= n) return NumbersRec(a + 1, n) + $"{a} ";
+ else return String.Empty;
+}
+
+Console.WriteLine(NumbersRec(a, n)); 
+
+int ReadInt(string message)
+{
+Console.WriteLine(message);
+return Convert.ToInt32(Console.ReadLine());
+}
