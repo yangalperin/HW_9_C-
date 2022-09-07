120d4ab292a4c7fc94d2bdaeafdf69bb4ec51f29
Yan <yangalperin@gmail.com>
2022-09-07 08:17:40 +0300

    task3

---
+++ Task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int m = ReadInt("Enter m number: ");
+int n = ReadInt("Enter n number: ");
+
+int SumRec(int m, int n)
+{
+    if (n == 0) return 0;
+    return n + SumRec(m, n - 1);
+}
+
+Console.WriteLine(SumRec(m, n));
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
