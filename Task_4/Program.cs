2e3615fbf40c4a4eec2402c5ad099c532c08f5ec
Yan <yangalperin@gmail.com>
2022-09-08 09:18:35 +0300

    task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу вычисления функции Аккермана
+int a = ReadInt("Enter a number: ");
+int b = ReadInt("Enter b number: ");
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Akkerman(m - 1, 1);
+    else
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+}
+Console.WriteLine(Akkerman(a,b));
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
